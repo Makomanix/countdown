09401126077f518e511f4c7b8aa7a3122ad3c463
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2024-05-07 11:41:14 -0400

    createPortal to relocate ResultModal in DOM

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useRef, forwardRef, useImperativeHandle } from 'react'
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal({ 
 targetTime, remainingTime, onReset, score }, ref) {
@@ -15,7 +16,7 @@ targetTime, remainingTime, onReset, score }, ref) {
     }
   })
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className='result-modal' onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
@@ -28,7 +29,8 @@ targetTime, remainingTime, onReset, score }, ref) {
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
   )
 })
 
